ffde4266f52bd7743913a5c2ca7fe4cc0e519753
Екатерина <afania41@gmail.com>
2025-05-04 15:39:46 +0400

    Add favorite quotes

--- index.js
+++ index.js
@@ -3,14 +3,32 @@ import quotes from './quote.js';
 const quoteElement = document.getElementById('quote');
 const authorElement = document.getElementById('author');
 const generateBtn = document.getElementById('generate-btn');
+const favoriteBtn = document.getElementById('favorite-btn');
 
+let currentQuoteIndex = null; 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
   const quote = randomQuote.quote;
   const quoteAuthor = randomQuote.author;
   quoteElement.textContent = quote;
   authorElement.textContent = quoteAuthor;
+  updateFavoriteButton();
+}
+
+function addQuoteToFavorites() {
+  if (currentQuoteIndex == null) return;
+ quotes[currentQuoteIndex].isFavorite = !quotes[currentQuoteIndex].isFavorite;
+ updateFavoriteButton();  
+}
+
+function updateFavoriteButton() {
+  if (currentQuoteIndex != null && quotes[currentQuoteIndex].isFavorite) {
+    favoriteBtn.classList.add('active');
+  } else {
+    favoriteBtn.classList.remove('active');
+  }
 }
 
 generateBtn.addEventListener('click', generateRandomQuote);
+favoriteBtn.addEventListener('click', addQuoteToFavorites);
